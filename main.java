4afad228d34a8728c35ffb01bb36932456ba4d43
DarkKnight8034 <79764769+DarkKnight8034@users.noreply.github.com>
2021-11-06 11:55:45 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package io.github.DarkKnight8034.Plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin
+{
+
+  @Override
+  public void onEnable()
+  {
+  
+    getLogger().info("Plugin is live!");
+  
+  }
+
+}
