3b0684a881123a0ee18f890e942d413b92f0ac60
DarkKnight8034 <darkknight8034@gmail.com>
2021-11-07 08:16:43 -0500

    first commit

---
+++ test_plugin/src/main/java/io/github/darkknight8034/plugin/Main.java
@@ -0,0 +1,14 @@
+package io.github.darkknight8034.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin enabled!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin disabled! :(");
+    }
+}
